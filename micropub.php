3a5cf2d69a4b251b9a5a8786a3c28b9deaf3d0d2
Ryan Barrett <git@ryanb.org>
2016-10-23 20:21:11 -0700

    change insufficient scope error from 403 to 401
    https://micropub.rocks/server-tests/804

--- micropub.php
+++ micropub.php
@@ -174,7 +174,7 @@ class Micropub {
 				$code, 'invalid access token: ' . $body );
 		} elseif ( ! in_array( 'post',  $scopes ) && ! in_array( 'create',  $scopes ) ) {
 			static::handle_authorize_error(
-				403, 'access token is missing post scope; got `' . $params['scope'] . '`' );
+				401, 'access token is missing post or create scope; got `' . $params['scope'] . '`' );
 		}
 
 		parse_str( $body, $resp );
