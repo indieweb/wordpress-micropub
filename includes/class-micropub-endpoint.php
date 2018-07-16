8766eae36a78eb2d9a84f54f8650e448f4ea7588
David Shanske <david@shanske.com>
2018-07-16 18:53:46 -0500

    Use new syndicate function in all places called

--- includes/class-micropub-endpoint.php
+++ includes/class-micropub-endpoint.php
@@ -135,7 +135,7 @@ class Micropub_Endpoint {
 		$url = self::get( static::$input, 'url' );
 
 		// check that we support all requested syndication targets
-		$synd_supported = apply_filters( 'micropub_syndicate-to', array(), $user_id );
+		$synd_supported = self::get_syndicate_targets( $user_id );
 		$uids           = array();
 		foreach ( $synd_supported as $syn ) {
 			$uids[] = self::get( $syn, 'uid' );
@@ -217,14 +217,14 @@ class Micropub_Endpoint {
 		if ( ! $resp ) {
 			switch ( static::$input['q'] ) {
 				case 'config':
-					$resp = array( 
-							'syndicate-to' => static::get_syndicate_targets( $user_id ),
-							'media-endpoint' => rest_url( MICROPUB_NAMESPACE . '/media' )
-						);
-					break;	
+					$resp = array(
+						'syndicate-to'   => static::get_syndicate_targets( $user_id ),
+						'media-endpoint' => rest_url( MICROPUB_NAMESPACE . '/media' ),
+					);
+					break;
 				case 'syndicate-to':
 					// return syndication targets with filter
-					$resp          = array( 'syndicate-to' => static::get_syndicate_targets( $user_id ) );
+					$resp = array( 'syndicate-to' => static::get_syndicate_targets( $user_id ) );
 					break;
 				case 'category':
 					$resp = array_merge(
