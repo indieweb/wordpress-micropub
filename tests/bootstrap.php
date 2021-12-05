9dda4158285e6ebaae7fb613845a1ac05cb3c458
David Shanske <david@shanske.com>
2021-12-05 17:32:02 +0000

    Adjusted line again

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@
  * @package micropub
  */
 
-require dirname( __FILE__ ) . 'vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+require dirname( __FILE__, 2 ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 require dirname( __FILE__ ) . '/class-indieauth-plugin.php';
 
 // define( 'WP_DEBUG', false );
