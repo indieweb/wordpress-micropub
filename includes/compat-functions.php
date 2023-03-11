c6662698c51db135251c540d21e39d9eb491b94f
David Shanske <david@shanske.com>
2023-03-11 03:44:33 +0000

    Desimplify line as does not evaluate in PHP5.6 and add compat for functions to simplify code

--- includes/compat-functions.php
+++ includes/compat-functions.php
@@ -80,3 +80,23 @@ if ( ! function_exists( 'wp_timezone' ) ) {
 		return new DateTimeZone( wp_timezone_string() );
 	}
 }
+
+// Polyfill for pre-PHP 7.3.
+if ( ! function_exists( 'array_key_first' ) ) {
+	function array_key_first( array $arr ) {
+		foreach ( $arr as $key => $unused ) {
+			return $key;
+		}
+		return null;
+	}
+}
+
+// Polyfill for pre-PHP 7.3.
+if ( ! function_exists( 'array_key_last' ) ) {
+	function array_key_last( $array ) {
+		if ( ! is_array( $array ) || empty( $array ) ) {
+			return null;
+		}
+		return array_keys( $array )[ count( $array ) - 1 ];
+	}
+}
