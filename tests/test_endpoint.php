d3982cee2b5f16bd0f0d7ddba4e9eb26a82aee4a
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-03-28 14:25:01 +0100

    Fix test to account for updated property in update response

--- tests/test_endpoint.php
+++ tests/test_endpoint.php
@@ -478,8 +478,10 @@ EOF;
 		$this->assertEquals( 'bar', $tags[0]->name );
 		$mf2 = $this->query_source( $post->ID );
 		$this->assertArrayHasKey( 'published', $mf2['properties'] );
-		// We have confirmed it exists now compare everything but this.
+		$this->assertArrayHasKey( 'updated', $mf2['properties'] );
+		// We have confirmed they exist now compare everything but these.
 		unset( $mf2['properties']['published'] );
+		unset( $mf2['properties']['updated'] );
 		$this->assertEquals(
 			array(
 				'properties' => array(
